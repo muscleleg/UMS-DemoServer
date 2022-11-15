d353620419086a457a0ef6008b3cf5445021feb7
김재준 <51969892+muscleleg@users.noreply.github.com>
2022-11-15 14:43:51 +0900

    user 엔티티 삭제

--- src/main/java/com/okestro/ums/controller/UmsController.java
+++ src/main/java/com/okestro/ums/controller/UmsController.java
@@ -1,41 +1,57 @@
 package com.okestro.ums.controller;
 
-import com.okestro.ums.domain.Hub;
-import com.okestro.ums.domain.StatusEnum;
-import com.okestro.ums.domain.User;
-import com.okestro.ums.dto.HubDto;
-import com.okestro.ums.dto.ResponseDto;
-import com.okestro.ums.repository.user.UserRepository;
-import com.okestro.ums.service.HubService;
-import com.okestro.ums.service.UserService;
+import com.okestro.ums.domain.*;
+import com.okestro.ums.dto.*;
+import com.okestro.ums.service.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.nio.charset.Charset;
 
 @RequiredArgsConstructor
 @RestController
 public class UmsController {
-    private final UserService userService;
     private final HubService hubService;
+    private final MessengerService messengerService;
+    private final MailService mailService;
+    private final SmsService smsService;
     @PostMapping("/api/hub")
     public ResponseEntity<ResponseDto> sendHub(@Valid HubDto hubDto, BindingResult bindingResult){
-        User findUser = userService.findByUserIdAndUserName(hubDto.getUserId(), hubDto.getUserName());
-        if(findUser!=null && !bindingResult.hasErrors()){
-            Hub hub = Hub.createHub(hubDto.getMsg(), hubDto.getTitle(), findUser);
+        if(!bindingResult.hasErrors()){
+            Hub hub = Hub.createHub(hubDto.getMsg(), hubDto.getTitle(),hubDto.getUserId(),hubDto.getUserName());
             hubService.saveHub(hub);
             return ResponseDto.createResponseEntity(true,hubDto);
         }
         return ResponseDto.createResponseEntity(false,hubDto);
     }
-
+    @PostMapping("/api/messenger")
+    public ResponseEntity<ResponseDto> sendMessenger(@Valid MessengerDto messengerDto, BindingResult bindingResult){
+        if(!bindingResult.hasErrors()){
+            Messenger messenger = Messenger.createMessenger(messengerDto.getMsg(), messengerDto.getTitle(), messengerDto.getUserId(), messengerDto.getUserName());
+            messengerService.save(messenger);
+            return ResponseDto.createResponseEntity(true,messengerDto);
+        }
+        return ResponseDto.createResponseEntity(false,messengerDto);
+    }
+    @PostMapping("/api/mail")
+    public ResponseEntity<ResponseDto> sendMail(@Valid MailDto mailDto, BindingResult bindingResult){
+        if(!bindingResult.hasErrors()){
+            Mail mail = Mail.createMail(mailDto.getAddress(), mailDto.getContents(), mailDto.getTitle(), mailDto.getUserId(),mailDto.getUserName());
+            mailService.save(mail);
+            return ResponseDto.createResponseEntity(true,mailDto);
+        }
+        return ResponseDto.createResponseEntity(false,mailDto);
+    }
+    @PostMapping("/api/sms")
+    public ResponseEntity<ResponseDto> sendMail(@Valid SmsDto smsDto, BindingResult bindingResult){
+        if(!bindingResult.hasErrors()){
+            Sms sms = Sms.createSms(smsDto.getDestPhoneNumber(),smsDto.getMsg(),smsDto.getTitle(),smsDto.getUserId(), smsDto.getUserName());
+            smsService.save(sms);
+            return ResponseDto.createResponseEntity(true,smsDto);
+        }
+        return ResponseDto.createResponseEntity(false,smsDto);
+    }
 }
--- src/main/java/com/okestro/ums/repository/user/UserRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.okestro.ums.repository.user;
-
-import com.okestro.ums.domain.User;
-
-public interface UserRepositoryCustom {
-
-}
